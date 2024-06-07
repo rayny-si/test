c6857beb7b80f312ac9cdf87e70d323abb1d7674
rayny-si <justmailforelsa@gmail.com>
2024-06-07 13:57:24 +0630

    git commit

---
+++ test1.py
@@ -0,0 +1,68 @@
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
+import streamlit as st
+
+def teachable_machine_classification(img):
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
+
+    # Load the model
+    model = load_model("keras_Model.h5", compile=False)
+
+    # Load the labels
+    class_names = open("labels.txt", "r").readlines()
+
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    # Replace this with the path to your image
+    image = Image.open('test.png').convert("RGB")
+
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # Predicts the model
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Print prediction and confidence score
+    # print("Class:", class_name[2:], end="")
+    # print("Confidence Score:", confidence_score)
+
+    return class_name, confidence_score
+
+st.set_page_config(layout="wide")
+
+st.title("Test Wave Drawings for Parkinson's")
+
+input_img = st.file_uploader("Enter your Image", type=['jpeg', 'jpg', 'png'])
+
+if input_img is not None:
+    if st.button("Test"):
+
+        col1, col2, col3 =st.columns([1,1,1])
+
+        with col1:
+            st.info("Your uploaded image")
+            st.image(input_img, use_column_width=True)
+
+        with col2:
+            st.info("Your result")
+            
+        with col3:
+            st.info("Information related to disease")
