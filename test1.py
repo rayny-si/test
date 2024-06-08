d72e92d2b68236f5cafc2ca1ecb035d0440c021f
rayny-si <85389874+rayny-si@users.noreply.github.com>
2024-06-08 13:04:30 +0630

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,3 @@
-import tensorflow as tf
 from tensorflow.python.keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
