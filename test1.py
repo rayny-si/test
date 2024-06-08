2bae9526425b9c1195d704599a43c06821f41853
rayny-si <justmailforelsa@gmail.com>
2024-06-08 13:00:18 +0630

    commit

--- test1.py
+++ test1.py
@@ -1,4 +1,5 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
+import tensorflow as tf
+from tensorflow.python.keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 import streamlit as st
